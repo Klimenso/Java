14e54206d9648793421d978b123f1e80d8e3a1ef
Klimenso <klimmaslovets@gmail.com>
2022-03-19 01:50:19 +0300

    did lesson2.task7

--- src/com/company/lesson2/TasksLesson2.java
+++ src/com/company/lesson2/TasksLesson2.java
@@ -81,3 +81,14 @@ class Task6 {
         }
     }
 }
+
+class Task7 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i < 100; i+=2)
+        {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
