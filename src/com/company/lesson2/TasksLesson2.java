a74237c4512449503eed6999f04f55c8b596e843
Klimenso <klimmaslovets@gmail.com>
2022-03-19 01:38:00 +0300

    did lesson2.task6

--- src/com/company/lesson2/TasksLesson2.java
+++ src/com/company/lesson2/TasksLesson2.java
@@ -71,3 +71,13 @@ class Task5 {
         }
     }
 }
+
+class Task6 {
+    public static void main(String[] args) {
+        for(int i = 2; i <= 100; i++) {
+            if( (i & 1) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
