91ec944c687f79411bb1650110f4e79b03de5206
Klimenso <klimmaslovets@gmail.com>
2022-04-02 23:06:19 +0300

    Make homework of lesson5

--- src/com/company/lesson58/Main.java
+++ src/com/company/lesson58/Main.java
@@ -1,5 +1,21 @@
 package com.company.lesson58;
 
 public class Main {
-        
+
+        public static void main(String[] args) {
+
+                Shape aCircle1 = new Circle("Red", 1, 2);
+                Shape aCircle2 = new Circle("Yellow", 5, 10);
+                Shape aRectangle1 = new Rectangle("Black", 2, 3);
+                Shape aRectangle2 = new Rectangle("White", 11, 9);
+
+
+
+                Shape[] shapes = {aCircle1, aCircle2, aRectangle1, aRectangle2};
+
+                for(Shape i : shapes) {
+                        i.draw();
+                }
+
+        }
 }
---
+++ src/com/company/lesson58/Rectangle.java
@@ -0,0 +1,22 @@
+package com.company.lesson58;
+
+public class Rectangle extends Shape {
+
+        private int x;
+        private int y;
+
+        public Rectangle() {
+
+        }
+
+        public Rectangle(String color, int x, int y) {
+               super(color);
+               this.x = x;
+               this.y = y;
+        }
+
+        @Override
+        public void draw() {
+                System.out.println("Rectangle" + "\nx: " + this.x + "\ny: " + this.y + "\nColor: " + this.getColor());
+        }
+}
