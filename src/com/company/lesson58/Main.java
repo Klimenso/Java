64a38d13e47d123fa5db6858ccabd460cf0461cc
Klimenso <klimmaslovets@gmail.com>
2022-04-06 00:55:28 +0300

    Make some tasks from lesson5-8

--- src/com/company/lesson58/Main.java
+++ src/com/company/lesson58/Main.java
@@ -1,12 +1,14 @@
 package com.company.lesson58;
 
 import com.company.lesson58.animals.*;
+import com.company.lesson58.diff_tasks.Aspirant;
+import com.company.lesson58.diff_tasks.Student;
 
 public class Main {
 
         public static void main(String[] args) {
 
-                Animal[] animals = {
+                /*Animal[] animals = {
                                         new Dog("Meet", "New York", "BigBoy", "Hasky", true),
                                         new Dog("Bones", "Moscow", "LittleBoy", "Mops", false),
                                         new Dog("Tomato's", "Praga", "Woman", "Nope", true),
@@ -26,6 +28,12 @@ public class Main {
 
                 for(Veterinarian patient : vet_patients) {
                         patient.treatAnimal();
+                }*/
+
+                Student[] students = { new Aspirant(), new Student(), new Aspirant(), new Student() };
+
+                for(Student member : students) {
+                        System.out.println(member.getScholarship());
                 }
 
         }
