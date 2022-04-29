f48ea64b3aefc7e75fe9954e723a962cfbae5ec1
Klimenso <klimmaslovets@gmail.com>
2022-04-30 01:24:28 +0300

    make lesson12(streams)

--- src/com/company/lesson12/Main.java
+++ src/com/company/lesson12/Main.java
@@ -1,7 +1,29 @@
 package com.company.lesson12;
 
+import com.company.lesson11.Car;
+
+import java.io.IOException;
+
 public class Main {
-        public static void main(String[] args ) {
+
+        public static void main(String[]args) {
+
+
+                try {
+
+                        Car newCar = (Car)SerializeHandler.deserialize("serizlizeCar");
+                        System.out.println(newCar);
+
+
+
+                } catch (IOException | ClassNotFoundException e) {
+
+                        e.getMessage();
+                }
+
+
 
         }
+
+
 }
